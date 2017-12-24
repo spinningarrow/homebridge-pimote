4cf42e58888ea5c287e402cfdf03d63f81bc19fb
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2017-12-24 18:38:21 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const request = require('request');
+const url = require('url');
+let Service, Characteristic;
+
+function mySwitch(log, config) {
+	this.log = log;
+	this.getUrl = url.parse(config['getUrl']);
+	this.postUrl = url.parse(config['postUrl']);
+}
+
+mySwitch.prototype = {
+	getSwitchOnCharacteristic: function (next) {
+		const me = this;
+		request({
+			url: me.getUrl,
+			method: 'GET',
+		}, function (error, response, body) {
+			if (error) {
+				me.log('STATUS: ' + response.statusCode);
+				me.log(error.message);
+				return next(error);
+			}
+			return next(null, "on");
+		});
+	},
+
+	setSwitchOnCharacteristic: function (on, next) {
+		const me = this;
+		request({
+			url: me.postUrl,
+			method: 'GET',
+		}, function (error, response) {
+			if (error) {
+				me.log('STATUS: ' + response.statusCode);
+				me.log(error.message);
+				return next(error);
+			}
+			return next();
+		});
+	},
+
+	getServices: function () {
+		let informationService = new Service.AccessoryInformation();
+		informationService
+			.setCharacteristic(Characteristic.Manufacturer, "Sahil Panasonic")
+			.setCharacteristic(Characteristic.Model, "Some TV model")
+			.setCharacteristic(Characteristic.SerialNumber, "123-456-789");
+
+		let switchService = new Service.Switch("My switch");
+		switchService
+			.getCharacteristic(Characteristic.On)
+			.on('get', this.getSwitchOnCharacteristic.bind(this))
+			.on('set', this.setSwitchOnCharacteristic.bind(this));
+
+		this.informationService = informationService;
+		this.switchService = switchService;
+		return [informationService, switchService];
+	}
+};
+
+module.exports = function (homebridge) {
+	Service = homebridge.hap.Service;
+	Characteristic = homebridge.hap.Characteristic;
+	homebridge.registerAccessory("homebridge-pimote", "MyAwesomeSwitch", mySwitch);
+};
+
